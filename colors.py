032e0dc24272acc4959f965780a3f79370672da9
David Pennington <david.pennington@uky.edu>
2014-04-22 12:22:10 -0400

    This is the DB connected version of the colorscheme

--- colors.py
+++ colors.py
@@ -3,20 +3,17 @@
 import cStringIO
 import urllib
 import os
+import json
 from PIL import Image
 from scipy.cluster import vq as sci
 from numpy import array
 import pytumblr
 from pprint import pprint as pp
+import psycopg2
 
 # fill this in first
-target = ""
-tumblr = pytumblr.TumblrRestClient(
-	'',
-	'',
-	'',
-	''
-)
+credentials = json.loads(open('tumblr_credentials.json', 'r').read())
+tumblr = pytumblr.TumblrRestClient(credentials["consumer_key"],credentials["consumer_secret"],credentials["oauth_token"],credentials["oauth_token_secret"])
 
 def get_points(img):
 	points = []
@@ -25,9 +22,9 @@ def get_points(img):
 		points.append([color[0],color[1],color[2]])
 	return points
 
-rtoh = lambda rgb: '#%s' % ''.join(('%02x' % p for p in rgb))
+rtoh = lambda rgb: '%s' % ''.join(('%02x' % p for p in rgb))
 
-def colorz(img, n=3, trim=8):
+def colorz(img, n=10, trim=32):
 	coords = [(x,y) for x in range(img.size[0]) for y in range(img.size[1])]
 	
 	for coord in coords:
@@ -49,29 +46,95 @@ def colorz(img, n=3, trim=8):
 	return map(rtoh, rgbs)
 
 def main():
-	info = tumblr.blog_info(target)
-	posts = info["blog"]["posts"]
-	step = 10
-	colormap = {}
+	conn_string = "host='localhost' dbname='cs585' user='cs585' "
+	db_conn = psycopg2.connect(conn_string)
+	cursor = db_conn.cursor()
+	limit=100
+	offset=2
 
-	for offset in range(0, posts, step):
-		print("{0} - {1}".format(offset,offset+step))
-		posts = tumblr.posts(target,limit=step,offset=offset)["posts"]
-		for post in posts:
-			if post["type"] == u'photo':
-				for photo in post["photos"]:
-					try:
-						url = photo["original_size"]["url"]
-						f = cStringIO.StringIO(urllib.urlopen(url).read())
-						img = Image.open(f)
-						colors = colorz(img,trim=32)
-						print(url, colors)
+	while True:
+		cursor.execute("SELECT blog_name FROM blog LIMIT %s OFFSET %s;", (limit, limit*offset))
 
-						for color in colors:
-							colormap[color] = colormap.get(color,0) + 1
-					except:
-						continue
-		pp(colormap)
+		for record in cursor:
+			print record
+			#set Target from DB
+			target=record[0]
+			info = tumblr.blog_info(target)
+			try:
+				posts = info["blog"]["posts"]
+			except:
+				continue
+			step = 10
+			count = 0
+			done = False
+			colormap = {}
+
+			for notOffset in range(0, posts, step):
+				if done:
+					break
+				#print("{0} - {1}".format(offset,offset+step))
+				posts = tumblr.posts(target,limit=step,offset=notOffset)["posts"]
+				for post in posts:
+					if post["type"] == u'photo':
+						for photo in post["photos"]:
+							try:
+								photolist = photo["alt_sizes"]
+								for photo in photolist:
+									if photo["width"] == 400:
+										url = photo["url"]
+								if url == None:
+									break
+								try:
+									f = cStringIO.StringIO(urllib.urlopen(url,timeout=1).read())
+									img = Image.open(f)
+									colors = colorz(img,trim=32)
+										#print(url, colors)
+									count = count + 1
+								except:
+									continue
+
+								for color in colors:
+									colormap[color] = colormap.get(color,0) + 1
+								if count == 25:
+									done = True
+							except Exception,e:
+								continue
+			
+			if not colormap:
+				continue
+			colormap = sorted(colormap, key=colormap.get, reverse=True)
+			# for color in colormap:
+			# 	print color	
+
+			cursor2 = db_conn.cursor()
+			#send to DB
+			try:	
+				cursor2.execute("insert into colors values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",
+					(target,
+					colormap[0],
+					colormap[1],
+					colormap[2],
+					colormap[3],
+					colormap[4],
+					colormap[5],
+					colormap[6],
+					colormap[7],
+					colormap[8],
+					colormap[9],
+					)
+					)	
+				db_conn.commit()
+			except Exception as e:
+				print ("DB Fail - ",e)
+				db_conn.rollback()
+
+		offset = offset + 1
+		print offset
+				
+	db_conn.commit()
+	cursor.close()
+	cursor2.close()
+	db_conn.close()
 
 if __name__ == "__main__":
 	main()
