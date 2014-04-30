8a5ef09b2776f825ebf293ee0593855f14f47ad7
graczyk <nick.graczyk@gmail.com>
2014-04-30 15:29:01 -0400

    This now works, need to do smarter thing for LIKE to get better results

--- site.py
+++ site.py
@@ -10,11 +10,17 @@ def results():
 	conn_string = "host='localhost' dbname='cs585' user='cs585' "
 	db_conn = psycopg2.connect(conn_string)
 	cursor = db_conn.cursor()
-	cursor.execute("SELECT blog_name FROM colors WHERE blog_name = '%s';", (user_entry))
-	print cursor.record
+	cursor.execute("SELECT blog_name FROM colors WHERE blog_name = '{0}';".format(user_entry))
 	#check to see if blog isn't in the color DB, if so return page with error message
-	
-	return render_template("results.html", blog = user_entry)
+	result = cursor.fetchone()
+	if result is None:
+		return render_template("results.html", blog = user_entry, found = False)
+	cursor.execute("SELECT * FROM colors WHERE blog_name = '{0}';".format(user_entry))
+	user_blog = cursor.fetchone()
+	cursor.execute("SELECT * FROM colors WHERE color1 LIKE '{0}'".format(user_blog[1]))
+	results = cursor.fetchall()
+	print results
+	return render_template("results.html", blog = user_entry, found = True, target = user_blog, results = results)
 
 @app.route('/')
 def home():
