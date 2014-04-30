a7f25ac85453b1ab40e06c5341d6accfa76bc70b
graczyk <nick.graczyk@gmail.com>
2014-04-30 16:06:09 -0400

    added randomness. lgtm

--- site.py
+++ site.py
@@ -1,5 +1,6 @@
 from flask import *
 import psycopg2
+from random import shuffle
 
 app = Flask(__name__)
 
@@ -15,11 +16,13 @@ def results():
 	result = cursor.fetchone()
 	if result is None:
 		return render_template("results.html", blog = user_entry, found = False)
+
 	cursor.execute("SELECT * FROM colors WHERE blog_name = '{0}';".format(user_entry))
 	user_blog = cursor.fetchone()
-	cursor.execute("SELECT * FROM colors WHERE color1 LIKE '{0}'".format(user_blog[1]))
+	cursor.execute("SELECT * FROM colors WHERE color1 LIKE '{0}' OR color2 LIKE '{1}' OR color3 LIKE '{2}';".format(user_blog[1],user_blog[2],user_blog[3]))
 	results = cursor.fetchall()
-	print results
+	#instead of doing actual smart ranking, just shuffle the results
+	shuffle(results)
 	return render_template("results.html", blog = user_entry, found = True, target = user_blog, results = results)
 
 @app.route('/')
@@ -27,4 +30,4 @@ def home():
 	return render_template("home.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, port=8080, host='0.0.0.0')
